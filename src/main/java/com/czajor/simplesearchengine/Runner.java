2f0e98754ad26b73a72506b28ddc8c01b7797652
radekczdev <radekcz.dev@gmail.com>
2019-08-17 17:53:57 +0200

    Created Application UI runner; added try-catch clause for checking if index exists; added instructions shown during app runtime

---
+++ src/main/java/com/czajor/simplesearchengine/Runner.java
@@ -0,0 +1,52 @@
+package com.czajor.simplesearchengine;
+
+import com.czajor.simplesearchengine.domain.Index;
+import com.czajor.simplesearchengine.repository.DocumentRepository;
+import com.czajor.simplesearchengine.repository.IndexRepository;
+import com.czajor.simplesearchengine.service.IndexingService;
+import com.czajor.simplesearchengine.service.InitiateDatabaseService;
+import com.czajor.simplesearchengine.service.PrintToConsoleService;
+import com.czajor.simplesearchengine.service.SearchIndexService;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class Runner {
+    private IndexRepository indexRepository;
+    private DocumentRepository documentRepository;
+    private InitiateDatabaseService databaseService = new InitiateDatabaseService();
+    private IndexingService indexingService = new IndexingService();
+    private SearchIndexService searchIndexService;
+
+    public Runner(IndexRepository indexRepository, DocumentRepository documentRepository) {
+        this.indexRepository = indexRepository;
+        this.documentRepository = documentRepository;
+        this.searchIndexService = new SearchIndexService(indexRepository, documentRepository);
+
+    }
+
+    public void run() {
+        PrintToConsoleService.printWelcome();
+        Scanner in = new Scanner(System.in);
+        String path = in.nextLine();
+        databaseService.addToDatabase(path, documentRepository);
+        indexingService.run(documentRepository, indexRepository);
+        do {
+            PrintToConsoleService.printProvideIndex();
+            String index = in.nextLine();
+            sort(searchIndexService, index);
+            PrintToConsoleService.printEnd();
+        } while(!in.nextLine().equals("yes"));
+    }
+
+    private void sort(SearchIndexService searchIndexService, String index) {
+        try {
+            Map<Long, Double> tfIdfMap = searchIndexService.sortByTfIdf(index);
+            Index curIndex = indexRepository.findIndexById(index).get();
+            curIndex.getTfIdfMap().putAll(tfIdfMap);
+            PrintToConsoleService.searchResult(curIndex);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/czajor/simplesearchengine/service/PrintToConsoleService.java
+++ src/main/java/com/czajor/simplesearchengine/service/PrintToConsoleService.java
@@ -10,11 +10,15 @@ public class PrintToConsoleService {
     public static void printWelcome() {
         System.out.println("*** Welcome to the search index ***");
         System.out.println("Application provides search of documents containing requested Index.");
-        System.out.println("Input should be placed in resources folder in .json file. One json element contains _body_ field with the String.");
-        System.out.println("Please provide path to the file with documents:");
+        System.out.println("Input .json file should be placed in resources folder. One json element contains _body_ field with the String.");
+        System.out.println("Please provide name of file with documents:");
     }
 
     public static void printProvideIndex() {
         System.out.println("Provide a word to be searched: ");
+    }
+
+    public static void printEnd() {
+        System.out.println("Exit: yes? yes, no? anyOtherString");
     }
 }
